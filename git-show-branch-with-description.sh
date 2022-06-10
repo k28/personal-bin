ecd8853377c81ba329a63cb7c25fcc7c3badd1ee
Kazuya Hatano <k28@me.com>
2022-06-10 13:12:07 +0900

    update

--- git-show-branch-with-description.sh
+++ git-show-branch-with-description.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git for-each-ref --format='%(refname)' | grep remotes | sed 's/.*origin\///' | grep -v HEAD | xargs -I@ bash -c "printf '%s  ' @ ; git config branch.@.description | tr -d '\n'; echo ''"
+git for-each-ref --format='%(refname)' | grep heads | sed 's/.*heads\///' | xargs -I@ bash -c "printf '%s  ' @ ; git config branch.@.description | tr -d '\n'; echo ''"
 
